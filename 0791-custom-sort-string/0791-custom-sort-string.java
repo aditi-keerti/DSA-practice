e8a01e16d231cf84daa375ea07a49dc3d5d58840
Aditi Bhadoriya <76037392+aditi-keerti@users.noreply.github.com>
2024-03-11 23:40:44 +0530

    Time: 2 ms (62.70%), Space: 41.3 MB (85.81%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String customSortString(String order, String s) {
+       // Create a frequency table
+        Map<Character, Integer> freq = new HashMap<>();
+
+        // Initialize frequencies of letters
+        // freq[c] = frequency of char c in s
+        int N = s.length();
+        for (int i = 0; i < N; i++) {
+            char letter = s.charAt(i);
+            freq.put(letter, freq.getOrDefault(letter, 0) + 1);
+        }
+
+        // Iterate order string to append to result
+        int K = order.length();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < K; i++) {
+            char letter = order.charAt(i);
+            while (freq.getOrDefault(letter, 0) > 0) {
+                result.append(letter);
+                freq.put(letter, freq.get(letter) - 1);
+            }
+        }
+
+        // Iterate through freq and append remaining letters
+        // This is necessary because some letters may not appear in `order`
+        for (char letter : freq.keySet()) {
+            int count = freq.get(letter);
+            while (count > 0) {
+                result.append(letter);
+                count--;
+            }
+        }
+
+        // Return the result
+        return result.toString(); 
+    }
+}
