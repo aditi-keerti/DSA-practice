eefb5578d0590c07d402380a54e3a581f1537d0d
Aditi Bhadoriya <76037392+aditi-keerti@users.noreply.github.com>
2024-03-23 23:50:04 +0530

    Time: 57 ms (26.20%), Space: 49 MB (27.61%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+   return {
+        toBe: (value) => {
+            if (value === val) {
+                return  true ;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: (value) => {
+            if (value !== val) {
+                return  true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    }; 
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
